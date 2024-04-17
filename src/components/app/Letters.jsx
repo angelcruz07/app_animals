e13ba9d84e7e7d3bdde8b8e788b75d4cc907e117
angelcruz07 <angelcruzsanchez126@gmail.com>
2024-04-16 22:07:40 -0600

    Add: component letters

--- src/components/app/Letters.jsx
+++ src/components/app/Letters.jsx
@@ -1,9 +1,38 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, FlatList } from 'react-native'
+import Alphabet from '../../data/letters'
 
 export const Letters = () => {
 	return (
-		<View>
-			<Text>Letras</Text>
+		<View style={styles.container}>
+			<FlatList
+				data={Alphabet}
+				keyExtractor={(item) => item.letter}
+				renderItem={({ item: letter }) => (
+					<View style={styles.cardLetter}>
+						<Text style={styles.letter}>{letter.letter}</Text>
+					</View>
+				)}
+			/>
 		</View>
 	)
 }
+
+const styles = StyleSheet.create({
+	container: {
+		marginTop: 100
+	},
+	cardLetter: {
+		backgroundColor: '#107E7D',
+		width: 200,
+		height: 200,
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginTop: 10,
+		borderRadius: 10
+	},
+	letter: {
+		fontWeight: 'bold',
+		color: '#fff',
+		fontSize: 80
+	}
+})
